3e37b08f51b1ba99095b83cb4859baadfeae8c2b
Kamal Singh <mechengg.kamal@outlook.com>
2024-05-08 20:24:03 -0600

    feat: add password hashing and createing user inside database

--- actions/registerAction.ts
+++ actions/registerAction.ts
@@ -1,8 +1,12 @@
 'use server';
 
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
 
+import { db } from '@/lib/db';
+
 import { RegisterSchema } from '@/lib/validations/auth';
+import { getUserbyEmail } from '@/data/user';
 
 export const registerAction = async (
   values: z.infer<typeof RegisterSchema>
@@ -13,5 +17,26 @@ export const registerAction = async (
   if (!validatedFields.success) {
     return { error: 'Invalidated fields!' };
   }
-  return { success: 'Email sent!' };
+
+  const { email, password, firstName, lastName } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserbyEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email.
+
+  return { success: 'User account created!' };
 };
