17135f0d173033abb492c03f52bd50fbbe1ca450
Kamal Singh <mechengg.kamal@outlook.com>
2024-06-02 20:53:06 -0600

    Fix: Marketing typo.

--- components/marketing/hero/hero1.tsx
+++ components/marketing/hero/hero1.tsx
@@ -9,11 +9,11 @@ const Hero1 = () => {
       <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl'>
         <span className='block xl:inline'>Starter template built with </span>{' '}
         <span className='block text-orange-600 xl:inline'>
-          NextJS . Drizzle . NextAuth
+          NextJS . Prisma . NextAuth
         </span>
       </h1>
       <p className='mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
-        Starter template build with NextJS, drizzle, nextAuth, Shadcn, Tailwind.
+        Starter template build with NextJS, prisma, nextAuth, Shadcn, Tailwind.
       </p>
       <div className='mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8 gap-4'>
         <LoginButton>
